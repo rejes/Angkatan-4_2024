7724c9eb4a9d342a98c96550c5aa42076a8ab4c8
azzamreymizar <areymizar@gmail.com>
2025-05-15 07:48:10 +0700

    project

---
+++ Percobaan.py
@@ -0,0 +1,23 @@
+# a = 10 
+# print (a)
+# a = "Yogyakarta"
+# print (a)
+
+# Menetapkan Nilai Variabel
+# a,b,c = 10,12,14
+# print(a)
+# print(b)
+# print(c)
+
+# Menetapkan Nilai Variable
+# a=b=c = 100
+# print(a)
+# print(b)
+# print(c)
+
+# Variable dan keluaran  
+# a= "Belajar"
+# print(a+"python") 
+
+
+
---
+++ pengondisian.py
@@ -0,0 +1,14 @@
+nilai = 84 
+
+if nilai >= 90:
+    print("Nilai kamu A")
+    
+elif nilai >= 80:
+    print("Nilai kamu B")
+
+elif nilai >= 70:
+    print("Nilai kamu C")
+
+else:
+    print("Kamu harus belajar lagi")        
+
