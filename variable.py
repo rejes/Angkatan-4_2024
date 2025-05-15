5fb8ddbbfaef8b41387b31dba438392941b23dde
KunSyafei <syafeikun@gmail.com>
2025-05-15 08:46:44 +0800

    tugas

---
+++ variable.py
@@ -0,0 +1,20 @@
+i = ("kun syafei")
+print (i)  
+
+a=10
+print(a)
+a="jogja"
+print(a)
+
+a,b,c = 10,12,13
+print(a)
+print(b)
+print(c)
+
+a=b=c =100
+print(a)
+print(b)
+print(c)
+
+b="belajar"
+print(b+ "pyton")
