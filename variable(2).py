9233457242fc7723b87ac4593ac653dcfafc2703
Mimissiee <growfitzgt@gmail.com>
2025-05-15 09:05:37 +0800

    py

---
+++ variable(2).py
@@ -0,0 +1,21 @@
+# Membuat variable
+a=10
+print(a)
+a="yogyakarta"
+print(a)
+
+# Menetapkan nilai variable
+a,b,c = 10,12,14
+print(a)
+print(b)
+print(c)
+
+# Menetapkan nilai variable
+a=b=c =100
+print(a)
+print(b)
+print(c)
+
+# Variable dan Keluaran
+a="Belajar"
+print(a+"Python")
---
+++ variable.py
@@ -0,0 +1,5 @@
+# Pengertian Variable tempat penyimpanan data yang dapat berubah selama program dijalankan.
+
+Informatika = "ammar"
+
+print(Informatika)
