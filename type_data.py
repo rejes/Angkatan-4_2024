219d20d1675ea39f23babe196b3ff544265d62cb
Richelieu <abiyyudzakiraghib@gmail.com>
2025-05-15 08:39:39 +0800

    tugas bukhoryraghib

---
+++ type_data.py
@@ -0,0 +1,26 @@
+
+# x=10
+# y=5
+# panjang=1000
+# print("data = ", x,", adalah tipe : ", type(x))
+
+# data_float = 1.5
+# print("data = ", data_float, ", adalah bertipe : ", type(data_float))
+
+# data_string = "Ningrum"
+# print("data = ", data_string, ", adalah bertipe : ", type(data_string))
+
+# data_bool = True
+# print("data = ", data_bool, ", adalah bertipe : ", type(data_bool))
+
+# data_complex = complex(5,6)
+# print("data = ", data_complex, ", adalah bertipe : ", type(data_complex))
+
+# from ctypes import c_double
+# data_c_double = c_double(10.5)
+# print("data = ", data_c_double, ", adalah bertipe : ", type(data_c_double))
+
+PHI = 3.14
+print (PHI)
+keliling = PHI * 12.1
+print (keliling)
