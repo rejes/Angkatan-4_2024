eb47c0ca210a329f11aa8e069ede21c25585bf63
Nama Kamu <atha68345@gmail.com>
2025-05-15 08:53:02 +0800

    tugas

---
+++ coba.py
@@ -0,0 +1,23 @@
+# a,b,c = 10,11,12
+# print(a)
+# print(b)
+# print(c)
+
+# a=b=c = 1000
+# print(a)
+# print(b)
+# print(c)
+
+# a="belajar"
+# print(a+"python")
+
+# a=("kelas 10-a")
+# b=("kelas 10-A")
+
+# print(a)
+# print(b)
+
+PHI = 3.14
+print(PHI)
+keliling = PHI * 12.1
+print(keliling)
---
+++ variable.py
@@ -0,0 +1,8 @@
+i = (1234567890)
+a = ("sya lagi belajar python")
+b = (5)
+c = (5)
+
+print (b+c)
+print (i)
+print (a)
